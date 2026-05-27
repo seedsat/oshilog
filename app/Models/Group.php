9a2a8e5ed43bbacecd8067d33431b30c3fb486c7
seedsat <84009566+seedsat@users.noreply.github.com>
2026-05-27 11:15:15 +0900

    feat: 全モデルにリレーションを定義

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    // 書き込み許可するカラム
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+    // グループは複数のメンバーを持つ
+    public function members()
+    {
+        return $this->hasMany(Member::class);
+    }
+
+    // グループは複数の支出記録を持つ
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
